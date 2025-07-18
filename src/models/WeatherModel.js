eb0c44e740e28d57e80d0f59007d00523bd9a7a0
Kenneth Buyck <kenneth.buyck@gmail.com>
2025-07-18 09:44:27 +0200

    added files to the maps, added WeatherService and WeatherModel code, changed Wheater to Weather

---
+++ src/models/WeatherModel.js
@@ -0,0 +1,9 @@
+class WheaterModel {
+  constructor(city, temp, condition) {
+    this.city = city;
+    this.temp = temp;
+    this.condition = condition;
+  }
+}
+
+module.exports = WheaterModel;
---
+++ src/services/WeatherService.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+require('dotenv').config();
+
+class WeatherService {
+    async fetchWheater(city) {
+
+        // OpenWeather API URL, set wheater variable from model and get API key from .env file
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.WHEATER_API_KEY}`;
+
+        // Try/Catch block (always catch errors!), get the weather for given city
+        try {
+            const response = await axios.get(url);
+            const data = response.data;
+
+            const res = await axios.get(url);
+            return res.data;
+        } 
+        catch (error) {
+            console.error('Error fetching weather data:', error);
+            throw new Error('Could not fetch weather data');
+        }
+    }
+}
+
+module.exports = new WheaterService();
